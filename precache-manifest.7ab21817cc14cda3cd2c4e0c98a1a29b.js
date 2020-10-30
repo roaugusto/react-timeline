6fd110c87847647bf8b92130561ae1b3bd4869e4
Rodrigo Santos <ro.augusto@gmail.com>
2020-10-30 20:30:06 -0300

    Updates

---
+++ precache-manifest.7ab21817cc14cda3cd2c4e0c98a1a29b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532adfe26d013959e88404e2eb37e7bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d969300126ad8003589",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "c131e8f743a8f5eda734",
+    "url": "./static/js/2.cf687886.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.cf687886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d969300126ad8003589",
+    "url": "./static/js/main.5b38bf5e.chunk.js"
+  },
+  {
+    "revision": "886fdc33e4609201ebc5",
+    "url": "./static/js/runtime-main.5134e5b4.js"
+  }
+]);
